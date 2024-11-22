46792e0a729ea45e7139f64c8dde57526dcc4461
C0SMICSHARK <Xanderrowley42@gmail.com>
2024-11-22 15:56:50 +0000

    Script Error's fixed
    Fixed Error's within the script.

--- Assets/Scripts/NoiseMapGeneration.cs
+++ Assets/Scripts/NoiseMapGeneration.cs
@@ -5,42 +5,36 @@ using UnityEngine;
 
 public class NoiseMapGeneration : MonoBehaviour
 {
-    
+
     public float[,] GenerateNoiseMap(int mapDepth, int mapWidth, float scale)
     {
         //with the mapDeath and mapWidth coordinates
-                float[,] noiseMap = new float[mapDepth, mapWidth];
-
-        for (int zIndex =0; zIndex < mapDepth; zIndex++)
+        float[,] noiseMap = new float[mapDepth, mapWidth];
+        for (int zIndex = 0; zIndex < mapDepth; zIndex++)
         {
-            for (int xIndex =0; xIndex < mapWidth; xIndex++)
+            for (int xIndex = 0; xIndex < mapWidth; xIndex++)
             {
-                // calculate sample indices based on the coordinates and the scale.
+                // calculate sample indices based on the coordinates and the scale
                 float sampleX = xIndex / scale;
                 float sampleZ = zIndex / scale;
-
-                // generate noise value using perlin noise.
+                // generate noise value using PerlinNoise
                 float noise = Mathf.PerlinNoise(sampleX, sampleZ);
-
                 noiseMap[zIndex, xIndex] = noise;
             }
         }
-    
         return noiseMap;
     
-    
-    
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    // Start is called before the first frame update
-    void Start()
+}
+
+
+
+
+
+
+
+
+// Start is called before the first frame update
+void Start()
     {
         
     }
--- Assets/Scripts/TileGeneration.cs
+++ Assets/Scripts/TileGeneration.cs
@@ -1,52 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TileGeneration : MonoBehaviour
 {
-
     [SerializeField]
     NoiseMapGeneration noiseMapGeneration;
-
     [SerializeField]
     private MeshRenderer tileRenderer;
-
     [SerializeField]
     private MeshFilter meshFilter;
-
-   [SerializeField]
-   private MeshCollider meshCollider;
-
+    [SerializeField]
+    private MeshCollider meshCollider;
     [SerializeField]
     private float mapScale;
-    
-    
-    // Start is called before the first frame update
+
+
+    float[,] noiseMap = new float[mapDepth, mapWidth];
+
+
+    public static int mapDepth { get; private set; }
+    public static int mapWidth { get; private set; }
+
     void Start()
     {
         GenerateTile();
     }
-
-   void GenerateTile()
+    
+    void GenerateTile()
     {
-        // Calculate tile depth and width based on the mesh Vertices
+        // calculate tile depth and width based on the mesh vertices
         Vector3[] meshVertices = this.meshFilter.mesh.vertices;
         int tileDepth = (int)Mathf.Sqrt(meshVertices.Length);
         int tileWidth = tileDepth;
-
-        // Calculate the offsets based on the tile position
-        float[,] heightMap = this.noiseMapGeneration.GenerateNoiseMap (tileDepth, tileWidth, this.mapScale);
-
-       
-        
+        // calculate the offsets based on the tile position
+        float[,] heightMap = this.noiseMapGeneration.GenerateNoiseMap(tileDepth, tileWidth, this.mapScale);
+        // generate a heightMap using noise
+        Texture2D tileTexture = BuildTexture(heightMap);
+        this.tileRenderer.material.mainTexture = tileTexture;
     }
     
-    
-    
-    
-    // Update is called once per frame
-    void Update()
+    private Texture2D BuildTexture(float[,] heightMap)
     {
-        
+        int tileDepth = noiseMap.GetLength(0);
+        int tileWidth = noiseMap.GetLength(1);
+        Color[] colorMap = new Color[tileDepth * tileWidth];
+        for (int zIndex = 0; zIndex < tileDepth; zIndex++)
+        {
+            for (int xIndex = 0; xIndex < tileWidth; xIndex++)
+            {
+                // transform the 2D map index is an Array index
+                int colorIndex = zIndex * tileWidth + xIndex;
+                float height = heightMap[zIndex, xIndex];
+                // assign as color a shade of grey proportional to the height value
+                colorMap[colorIndex] = Color.Lerp(Color.black, Color.white, height);
+            }
+        }
+        // create a new texture and set its pixel colors
+        Texture2D tileTexture = new Texture2D(tileWidth, tileDepth);
+        tileTexture.wrapMode = TextureWrapMode.Clamp;
+        tileTexture.SetPixels(colorMap);
+        tileTexture.Apply();
+        return tileTexture;
     }
 }
